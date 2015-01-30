5617d71b01037ffbc0a19141cb2ada7c70cd0180
Andrew Thomson <athomsonguy@gmail.com>
2015-01-29 23:24:22 -0800

    Initial commit of types.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='types',
+      version='0.1',
+      description='library which helps people constrain python functions by type.',
+      url='http://github.com/andrewguy9/types',
+      author='andrew thomson',
+      author_email='athomsonguy@gmail.com',
+      license='MIT',
+      packages=['types'],
+      entry_points = {
+        'console_scripts': [
+          ],
+      },
+      zip_safe=False)

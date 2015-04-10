c66bbfffbea0a98b004da8bdeb2c3015fdcddb95
Andrew Thomson <athomsonguy@gmail.com>
2015-04-10 15:25:50 -0700

    rename package

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 tests_require = ['tox', 'pytest']
 
-setup(name='prototype',
+setup(name='func_prototypes',
       version='0.1',
       description='library which helps people constrain python functions by type.',
-      url='http://github.com/andrewguy9/prototype',
+      url='https://github.com/andrewguy9/func_prototypes',
       author='andrew thomson',
       author_email='athomsonguy@gmail.com',
       license='MIT',
-      packages=['prototype'],
+      packages=['func_prototypes'],
       tests_require=tests_require,
       extras_require={'test': tests_require},
       entry_points = {

e343cc5b5285ec7fcb2fbded7cf3c2e79f30ef61
Andrew Thomson <athomsonguy@gmail.com>
2015-04-23 22:05:15 -0700

    remove tests_require

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 from setuptools import setup
 
-tests_require = ['tox', 'pytest']
-
 setup(name='func_prototypes',
       version='0.1',
       description='library which helps people constrain python functions by type.',
@@ -10,8 +8,6 @@ setup(name='func_prototypes',
       author_email='athomsonguy@gmail.com',
       license='MIT',
       packages=['func_prototypes'],
-      tests_require=tests_require,
-      extras_require={'test': tests_require},
       entry_points = {
         'console_scripts': [
           ],

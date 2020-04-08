324919e05e0943d5eace7089b6f42a40fed90695
Andrew Thomson <athomsonguy@gmail.com>
2020-04-08 00:57:29 -0700

    Minor changes to artifacts/metadata.
    Version bump.
    New artifacts.
    Updated description metadata.
    Allow zip.

--- setup.py
+++ setup.py
@@ -1,25 +1,20 @@
 from setuptools import setup
 
-tests_require = ['tox', 'pytest']
-
 setup(
     name='func_prototypes',
-    version='0.4',
-    description='library which helps people constrain python functions by type.',
-    url='https://github.com/andrewguy9/func_prototypes',
-    author='andrew thomson',
+    version='0.5',
+    author='Andrew Thomson',
     author_email='athomsonguy@gmail.com',
-    license='MIT',
     packages=['func_prototypes'],
-    entry_points = {
-      'console_scripts': [
-        ],
-    },
-    tests_require=tests_require,
+    url='https://github.com/andrewguy9/func_prototypes',
+    license='MIT',
+    description='library which helps people constrain python functions by type.',
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Topic :: Software Development :: Libraries',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 2.7',
     ],
-    zip_safe=False)
+)
